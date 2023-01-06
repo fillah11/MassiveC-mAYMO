ddd962428162f98cea4992b7dfbd69ed3525b5f3
Aristodd <aristodemos.ad@gmail.com>
2023-01-06 23:16:31 +0700

    Daftar

--- app/src/main/java/com/fillah/massiveaha/one/DaftarAct.kt
+++ app/src/main/java/com/fillah/massiveaha/one/DaftarAct.kt
@@ -1,12 +1,37 @@
 package com.fillah.massiveaha.one
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.fillah.massiveaha.MainActivity
 import com.fillah.massiveaha.R
+import com.fillah.massiveaha.databinding.ActivityDaftarBinding
+import com.fillah.massiveaha.two.HomeAct
 
 class DaftarAct : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDaftarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_daftar)
+        binding = ActivityDaftarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.back.setOnClickListener {
+            finish()
+        }
+
+        binding.chkDaftar.setOnCheckedChangeListener { _, isChecked ->
+            binding.btnDaftar.isEnabled = isChecked
+            binding.btnDaftar.isClickable = isChecked
+        }
+
+        binding.btnDaftar.setOnClickListener {
+            val intent = Intent(this, HomeAct::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
