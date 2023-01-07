f75cdc2d1c947816af636c6dcc0e40e6382a2306
Aristodd <aristodemos.ad@gmail.com>
2023-01-07 18:53:19 +0700

    Halaman tips

--- app/src/main/java/com/fillah/massiveaha/two/HomeAct.kt
+++ app/src/main/java/com/fillah/massiveaha/two/HomeAct.kt
@@ -1,12 +1,30 @@
 package com.fillah.massiveaha.two
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.fillah.massiveaha.R
+import com.fillah.massiveaha.databinding.ActivityHomeBinding
+import com.fillah.massiveaha.one.AboutAct
+import com.fillah.massiveaha.one.TipsAct
 
 class HomeAct : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnTips.setOnClickListener {
+            val intent = Intent(this, TipsAct::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnTentang.setOnClickListener {
+            val intent = Intent(this, AboutAct::class.java)
+            startActivity(intent)
+        }
     }
 }
