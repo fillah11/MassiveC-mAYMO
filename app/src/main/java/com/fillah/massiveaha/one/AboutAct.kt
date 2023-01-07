5faf21cf953afa8460f712c4f6dfc83ca808c834
Aristodd <aristodemos.ad@gmail.com>
2023-01-07 21:29:13 +0700

    Tentang Aplikasi

--- app/src/main/java/com/fillah/massiveaha/one/AboutAct.kt
+++ app/src/main/java/com/fillah/massiveaha/one/AboutAct.kt
@@ -2,11 +2,56 @@ package com.fillah.massiveaha.one
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.view.MenuItem
+import android.widget.Toast
 import com.fillah.massiveaha.R
+import com.fillah.massiveaha.databinding.ActivityAboutBinding
 
 class AboutAct : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_about)
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var actionBar = getSupportActionBar()
+
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+
+        binding.tvNotifikasi.setOnClickListener{
+            Toast.makeText(this, "Notifikasi", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tvSuara.setOnClickListener{
+            Toast.makeText(this, "Suara", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tvTema.setOnClickListener{
+            Toast.makeText(this, "Tema", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tvBahasa.setOnClickListener{
+            Toast.makeText(this, "Bahasa", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tvCadangan.setOnClickListener{
+            Toast.makeText(this, "Cadangan Google Drive", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.tvVersi.setOnClickListener{
+            Toast.makeText(this, "Versi", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return true
     }
 }
